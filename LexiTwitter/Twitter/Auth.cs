f37643b0b5203337befbe4bc637fe395d03e2ba4
Michael L <mikkelzuuu@gmail.com>
2017-03-12 23:44:11 +0100

    First Commit
    Added Auth for the account.

---
+++ LexiTwitter/Twitter/Auth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace LexiTwitter.Twitter
+{
+    public class Auth
+    {
+        Tokens AuthTokens = new Tokens();
+        public void Authenticate(MainWindow win)
+        {
+            var service = new TwitterService(AuthTokens.ConsumerKey, AuthTokens.ConsumerSecret);
+            service.AuthenticateWith(AuthTokens.AccessToken, AuthTokens.AccessTokenSecret);
+
+            var tweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions());
+            foreach (var tweet in tweets)
+            {
+                win.textboxforusers.AppendText(tweet.User.ScreenName + "\n");
+            }
+        }
+    }
+}
---
+++ LexiTwitter/Twitter/Tokens.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiTwitter.Twitter
+{
+    public class Tokens
+    {
+        public string ConsumerKey = "hV6HKMQPL1yzS5YGegeLBP7aG";
+        public string ConsumerSecret = "CpWSgydmXNGn5Z6RSQU90SP87UiGi8FWpGgaAq4hBvSsqokxuC";
+
+        public string AccessToken = "841043980011855872-i7r7XkW7qHCqXCvEw9XMwvEbZPmPNB1";
+        public string AccessTokenSecret = "h0iNzS1fbPxIxVUWbuBUgJmdvA5sQEyUlGfGqlCJ5wB1O";
+    }
+}
